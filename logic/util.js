cdc07432a95661f39bb8e4b948137b7b87155d7f
ctudose <44235992+ctudose@users.noreply.github.com>
2020-03-12 22:21:48 +0200

    Task 5

--- logic/util.js
+++ logic/util.js
@@ -1,3 +1,16 @@
 function Util() {
 
+    function calculateTotalDistributedPassengers(distributedPassengers) {
+        let totalDistributedPassengers = 0;
+        let value;
+        for (value in distributedPassengers) {
+           totalDistributedPassengers += distributedPassengers[value];
+        }
+        return totalDistributedPassengers;
+     }
+
+     return {calculateTotalDistributedPassengers};
+
 }
+
+module.exports = Util();
