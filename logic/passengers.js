fbd29b3ee8fca826e25ce97b28e39aed27b085a6
ctudose <44235992+ctudose@users.noreply.github.com>
2020-03-12 21:57:47 +0200

    Task 4

--- logic/passengers.js
+++ logic/passengers.js
@@ -1,3 +1,19 @@
 function Passengers() {
 
+    function checkFlightCapacity(flightCapacity, passengersArray) {
+        let passengersNumber = 0;
+        let passengers;
+        for(passengers of passengersArray) {
+            passengersNumber += passengers;
+        }
+        if (passengersNumber > flightCapacity) {
+            throw new Error("Flight capacity (" + flightCapacity + ") exceeded. You have " + passengersNumber + " passengers.");
+        }
+        return passengersNumber;
+     }
+
+     return {checkFlightCapacity};
+
 }
+
+module.exports = Passengers();
