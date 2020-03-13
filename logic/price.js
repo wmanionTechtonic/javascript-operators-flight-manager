b589dc5e202c34f9d2ccaef9349d6f76def26c64
ctudose <44235992+ctudose@users.noreply.github.com>
2020-03-13 12:25:39 +0200

    Task 8

--- logic/price.js
+++ logic/price.js
@@ -1,3 +1,16 @@
 function Prices() {
 
+    function calculateFinalPrice(basePrice, percentPassengerType, percentFlightType) {
+        let finalPrice = basePrice;
+
+        finalPrice *= 1 + percentPassengerType/100;
+        finalPrice *= 1 + percentFlightType/100;
+
+        return finalPrice.toFixed(2);
+    }
+
+    return {calculateFinalPrice};
+
 }
+
+module.exports = Prices();
