6a4e43a4a4921f962308fc32936df16f50893dde
wmanionTechtonic <wayne.manion@techtonic.com>
2020-10-30 11:11:51 -0500

    Fixed Part 2

--- logic/flights.js
+++ logic/flights.js
@@ -1,25 +1,29 @@
 function Flights() {
   function calculateNumberOfFlights(numPassengers, flightCapacity) {
-    if (numPassengers <= 0) {
+    if (numPassengers !== parseInt(numPassengers, 10))
       throw new Error("The number of passengers must be a positive integer value");
-    }
-    if (flightCapacity <= 0) {
-      throw new Error("The capacity of the flight must be a positive integer value");
-    }
+
+    if (numPassengers <= 0) 
+      throw new Error("The number of passengers must be a positive integer value");
+
+      if (flightCapacity !== parseInt(flightCapacity, 10))
+        throw new Error("The capacity of the flight must be a positive integer value");
+      if (flightCapacity <= 0)
+        throw new Error("The capacity of the flight must be a positive integer value");
 
     return Math.ceil(numPassengers / flightCapacity);
   }
 
   function checkAircraftRevision(distanceLimit, distances) {
     let totalDistance = distances.reduce((acc, cur) => acc + cur, 0);
-    if (totalDistance  <= distanceLimit / 2)
+    if (totalDistance  <= (distanceLimit / 2))
       return "The revision needs to be done within the next 3 months";
-    else if (totalDistance <= distanceLimit * 3 / 4)
-      return "The revision must be done within the next 2 months";
+    else if (totalDistance <= (distanceLimit * 3 / 4))
+      return "The revision needs to be done within the next 2 months";
     if (totalDistance <= distanceLimit)
-      return "The revision must be done within the next month"
+      return "The revision needs to be done within the next month"
     else
-      throw new Error("Total distance is over limit");
+      throw new Error("Aircraft is grounded.");
   }
 
   return {calculateNumberOfFlights, checkAircraftRevision};
